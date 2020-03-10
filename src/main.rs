351621520e1facb170bbf3ea0a0c99ff5e6c6f45
Chris <chris@chrislockard.net>
2020-03-10 15:35:51 -0400

    Create Config struct to show relationship of args

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
 fn main() {
     // Collect command arguments
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Error reading file");
+    let contents = fs::read_to_string(config.filename).expect("Error reading file");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    Config { query, filename }
 }
